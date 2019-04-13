adbf476b4b5baea1d86b93b7d90fb203d919c920
Iván <isancheznetwork@gmail.com>
2019-04-13 13:36:36 +0200

    feat(parser): Numbers parsed to ordinals

--- src/converter.js
+++ src/converter.js
@@ -1,11 +1,42 @@
 // @ts-check
+import {
+    concatOrdinal,
+    getUnit,
+    getTens,
+    getHundreds,
+    getThousands
+} from './parser';
 
-export default function converter (number) {
 
+/**
+ * @description This main function here gets the numbers and gender to proceed with the conversion
+ * @param number from 1 to 1000
+ * @param gender determine the end of any ordinal number
+ * */
+export default function converter (number, gender) {
+
+    let value;
     let ordinal = '';
+    const numPattern = /^[1-9]$/;
+    const numbers = number.toString();
+    const numListRev = numbers.split('').map((s) => parseFloat(s)).reverse();
 
-    if (number <= 10) {
-        ordinal = 'décimo';
+    const g = gender === 'female' ? 'a' : 'o';
+
+    for(let i = numbers.length; i > 0; i--) {
+        const n = numListRev[i-1];
+        if (numPattern.test(n.toString())) {
+            if (i === 4) {
+                value = getThousands(n, g);
+            } else if (i === 3) {
+                value = getHundreds(n, g);
+            } else if (i === 2) {
+                value = getTens(n, g);
+            } else if (i === 1) {
+                value = getUnit(n, g);
+            }
+            ordinal = concatOrdinal(ordinal, value);
+        }
     }
 
     return ordinal;
--- tests/index.test.js
+++ tests/index.test.js
@@ -4,7 +4,43 @@ import ordinal from '../src/index';
 
 describe('developer tests', () => {
 
-  test('Ordinal', () => {
+  test('Units', () => {
+    expect(ordinal(1)).toEqual('primero');
+    expect(ordinal(2)).toEqual('segundo');
+    expect(ordinal(3)).toEqual('tercero');
+    expect(ordinal(4)).toEqual('cuarto');
+    expect(ordinal(5)).toEqual('quinto');
+    expect(ordinal(6)).toEqual('sexto');
+    expect(ordinal(7)).toEqual('séptimo');
+    expect(ordinal(8)).toEqual('octavo');
+    expect(ordinal(9)).toEqual('noveno');
+  });
+
+  test('Tens', () => {
     expect(ordinal(10)).toEqual('décimo');
+    expect(ordinal(20)).toEqual('vigésimo');
+    expect(ordinal(30)).toEqual('trigésimo');
+    expect(ordinal(40)).toEqual('cuadragésimo');
+    expect(ordinal(50)).toEqual('quincuagésimo');
+    expect(ordinal(60)).toEqual('sexagésimo');
+    expect(ordinal(70)).toEqual('septuagésimo');
+    expect(ordinal(80)).toEqual('octogésimo');
+    expect(ordinal(90)).toEqual('nonagésimo');
+  });
+
+  test('Hundreds', () => {
+    expect(ordinal(100)).toEqual('centésimo');
+    expect(ordinal(200)).toEqual('ducentésimo');
+    expect(ordinal(300)).toEqual('tricentésimo');
+    expect(ordinal(400)).toEqual('cuadrigentésimo');
+    expect(ordinal(500)).toEqual('quingentésimo');
+    expect(ordinal(600)).toEqual('sexcentésimo');
+    expect(ordinal(700)).toEqual('septingentésimo');
+    expect(ordinal(800)).toEqual('octigentésimo');
+    expect(ordinal(900)).toEqual('noningentésimo');
+  });
+
+  test('Thousands', () => {
+    expect(ordinal(1000)).toEqual('milésimo');
   });
 });
